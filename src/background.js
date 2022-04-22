b31fe2d5d4029197af7632cdddaa99d2b16acda1
Jon <stricoff92@gmail.com>
2022-04-22 12:49:44 -0400

    iwp

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 
 
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("Hello! Havens-Chrome-Extention has been installed 👋");
+    console.log("Hello! bank-to-csv extension has been installed 👋");
     chrome.storage.local.set({onPage: false, running: false});
 });
